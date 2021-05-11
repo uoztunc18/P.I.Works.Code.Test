812909d21314008d656aaf096d10ccd071e5e622
uoztunc18 <72863333+uoztunc18@users.noreply.github.com>
2021-05-11 20:51:40 +0300

    Add files via upload
    Worked successfully, added comments

--- maxSumOnPyramid/maxSumOnPyramid.c
+++ maxSumOnPyramid/maxSumOnPyramid.c
@@ -1,3 +1,6 @@
+//Done by Umut Öztunç - uoztunc18@ku.edu.tr
+//Koç University - Computer Engineering & Economics
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,18 +11,14 @@ int findMaxPath();
 
 struct node {
     int data;
-    int level;
-    int rank;
     struct node* left;
     struct node* right;
 };
  
-struct node* newNode(int data, int level, int rank)
+struct node* newNode(int data)
 {
     struct node* node = (struct node*)malloc(sizeof(struct node));
     node->data = data;
-    node->level = level;
-    node->rank = rank;
     node->left = NULL;
     node->right = NULL;
     return (node);
@@ -30,48 +29,49 @@ int main()
     char line[256];	
     char *token;
     int levelCount = 0;
-    FILE *x = fopen("input.txt", "r");
-    if (x==NULL){
-        printf("ERROR");
+    
+    FILE *file = fopen("input.txt", "r");
+    if (file==NULL){
+        printf("No such file ?!");
 		return 0;
 	}
-	while (!feof(x)) {
-			fgets(line, 256, x);
+	while (!feof(file)) {      //getting the number of lines to determine the size of array which will store input numbers in the pyramid
+			fgets(line, 256, file);
 			levelCount++;
 	}
-	rewind(x);
+	rewind(file);
 
     struct node set[(levelCount*(levelCount+1))/2];
     int lvl = 0;
     int count = 0;
     int value;
     
-    fgets(line, 256, x);
+    fgets(line, 256, file);
     token = strtok(line, " ");
     value = atoi(token);
-    set[count] = *newNode(value, lvl, count);
+    set[count] = *newNode(value);
     count++;
     lvl++;
-    while (!feof(x)) {
-        fgets(line, 256, x);
+    while (!feof(file)) {
+        fgets(line, 256, file);
         token = strtok(line, " ");
         value = atoi(token);
-        set[count] = *newNode(value, lvl, count);
-        set[count-lvl].left = &set[count];
+        set[count] = *newNode(value);
+        set[count-lvl].left = &set[count];  //setting child-parent relation of the LEFTMOST number on the current line and its parent
         count++;
 
         token = strtok(NULL, " ");
         
         while (token != NULL) {
             value = atoi(token);
-            set[count] = *newNode(value, lvl, count);
+            set[count] = *newNode(value);
             
             token = strtok(NULL, " ");
             
             if (token == NULL) {
-                set[count-lvl-1].right = &set[count];
+                set[count-lvl-1].right = &set[count];   //setting child-parent relation of the RİGHMOST number on the current line and its parent
             } else {
-                set[count-lvl-1].right = &set[count];
+                set[count-lvl-1].right = &set[count];   //setting child-parent relation of the middle numbers on the current line and its (their) parent(s)
                 set[count-lvl].left = &set[count];
             }
             count++;
@@ -79,36 +79,42 @@ int main()
         lvl++;
     }
 
-    struct node accum = *newNode(set[0].data, 0, 0);
-    fillTree(&set[0], &accum);
+    struct node accum = *newNode(set[0].data);      //start point of the accumulation tree
+    fillTree(&set[0], &accum);      //discarding prime numbers and accumulating...
 
-    accum.data += findMaxPath(&accum);
+    accum.data += findMaxPath(&accum);      //replacing the data value of accum with the max sum path of the pyramid, which is the expected answer!
 
     printf("The maximum sum of the numbers from top to bottom is %d.\n", accum.data);
 
     return 0;
 }
 
+//This function creates a new, so-called "ACCUMULATION TREE", discarding the prime numbers
+//among the input numbers. The parameters are the start point of the input numbers' tree and a node having 
+//a node having created for a start point of the new tree, which will then, grow downwards, recursively. 
+//It does not create children nodes for ITSELF(accumulation tree) unless both children
+//nodes (in the original input tree) are prime number. 
 void fillTree(struct node *input, struct node *tree) {
 
     if (input->left != NULL && input->right != NULL) {
-        if ((checkPrime(input->left->data))&&(checkPrime(input->right->data))) {
-            //finishing
-        } else if (checkPrime(input->left->data)) {
-            tree->data += input->right->data;
-            fillTree(input->right, tree);
-        } else if (checkPrime(input->right->data)) {
+        if ((checkPrime(input->left->data))&&(checkPrime(input->right->data))) {     //in case of both children with prime numbers, do nothing, stop
+            
+        } else if (checkPrime(input->left->data)) {                                  //in case of one child with prime number, update the value of this node
+            tree->data += input->right->data;                                        //with the value itself plus the value of other non-prime child.
+            fillTree(input->right, tree);                                            //Thnaks to that, we accumulate the sum until encountering a seperation.
+        } else if (checkPrime(input->right->data)) {                                 //Then call itself recursively on the non-prime child.
             tree->data += input->left->data;
             fillTree(input->left, tree);
-        } else {
-            tree->left = newNode(input->left->data, (tree->level)+1, 0);
-            tree->right = newNode(input->right->data, (tree->level)+1, 0);
+        } else {                                                                     //in case of both children with non-prime numbers, it is time to 
+            tree->left = newNode(input->left->data);                                 //grow through different paths downwards.
+            tree->right = newNode(input->right->data);
             fillTree(input->left, tree->left);
             fillTree(input->right, tree->right);
         }
     }
 }
 
+//Prime number controler
 int checkPrime(int x) {
     int i;
     for (i = 2; i <= x - 1; i++) {
@@ -118,6 +124,7 @@ int checkPrime(int x) {
     if (i == x) return 1;
 }
 
+//For a node which has two children, this function returns the maximum among the children
 int returnMaxChild(struct node *node) {
     if (node->left->data >= node->right->data) {
         return node->left->data;
@@ -126,17 +133,20 @@ int returnMaxChild(struct node *node) {
     }
 }
 
+//This function takes the start point of the ACCUMULATION tree as a parameter; and finds the maximum sum, 
+//traversing all child-parent relationships recursively from bottom to top. In every call, it determines
+//the max among the children and return that number and move one step upward.
 int findMaxPath(struct node *node) {
-    if (node->left->left == NULL && node->right->right == NULL) {
+    if (node->left->left == NULL && node->right->right == NULL) {       //for the parent of both-childless nodes
         return returnMaxChild(node);
-    } else if (node->left->left == NULL) {
+    } else if (node->left->left == NULL) {                              //for the parent of one child-less node (left one is childless)
         node->right->data += findMaxPath(node->right);
         return returnMaxChild(node);
-    } else if (node->right->right == NULL) {
+    } else if (node->right->right == NULL) {                            //for the parent of one child-less node (right one is childless)
         node->left->data += findMaxPath(node->left);
         return returnMaxChild(node);
     } else {
-        node->left->data += findMaxPath(node->left);
+        node->left->data += findMaxPath(node->left);                    //for the parent of two children, both having children too
         node->right->data += findMaxPath(node->right);
         return returnMaxChild(node);
     }
